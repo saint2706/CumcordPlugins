5ecfa3ab016b2488da93cb0a7bab94070417d87a
Saint <arishabh748@gmail.com>
2022-05-07 14:47:00 +0530

    prntsc

---
+++ prnt-sc/src/index.js
@@ -0,0 +1,46 @@
+import { webpackModules } from "@cumcord/modules"
+
+let unpatch
+
+const random = () =>
+{
+	return Math.random().toString( 16 ).substring( 2, 8 )
+}
+
+export default {
+	onLoad ()
+	{
+		unpatch = cumcord.patcher.after(
+			"sendMessage",
+			webpackModules.findByProps( "sendMessage" ),
+			( args ) =>
+			{
+				if ( args[ 1 ].content.startsWith( "!prnt" ) )
+				{
+					const nextTest = args[ 1 ].content.replace( "!prnt", "" ).replace( " ", "" )
+					const nextTestInt = parseInt( nextTest )
+					if ( nextTestInt <= 5 )
+					{
+						console.log( "i have entered if statement" )
+						let message = ""
+						for ( let i = 0; i < nextTestInt; i++ )
+						{
+							const link = "https://prnt.sc/" + random()
+							message += link + "\n"
+						}
+						args[ 1 ].content = message
+					} else
+					{
+						const message = "https://prnt.sc/" + random()
+						args[ 1 ].content = message
+					}
+				}
+				return args
+			}
+		)
+	},
+	onUnload ()
+	{
+		unpatch()
+	},
+}
